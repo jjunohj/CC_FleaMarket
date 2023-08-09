83b7c61c1bbef71e0bcca2b1de435203756e877f
jjunohj <jjunohj@gmail.com>
2023-08-09 21:44:06 +0900

    Feat: 댓글 클릭시 작성자 알림

--- src/components/units/boardComment/list/BoardCommentList.presenter.tsx
+++ src/components/units/boardComment/list/BoardCommentList.presenter.tsx
@@ -3,10 +3,16 @@ import { getDate } from "../../../../commons/libraries/utils";
 import { IBoardCommentListUIProps } from "./BoardCommentList.types";
 
 export default function BoardCommentWriteUI(props: IBoardCommentListUIProps) {
+  const onClickInfo = (event) => {
+    alert(event.currentTarget.id + "님이 작성한 댓글입니다.");
+  };
+
+  // 이벤트 버블링이란 이벤트가 중첩되어 발생할 때, 가장 안쪽에 있는 요소에서 가장 바깥쪽에 있는 요소까지 이벤트가 전달되는 현상을 의미합니다.
+  // 이벤트 버블링을 막기 위해서는 이벤트 객체의 stopPropagation() 메서드를 사용합니다.
   return (
     <div>
       {props.data?.fetchBoardComments.map((el) => (
-        <S.ItemWrapper>
+        <S.ItemWrapper id={el.writer} onClick={onClickInfo}>
           <S.FlexWrapper>
             <S.Avatar src="/images/avatar.png" />
             <S.MainWrapper>
