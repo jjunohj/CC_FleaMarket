951fc0ce88e25385180402f97ff0f65cd10368e3
jjunohj <jjunohj@gmail.com>
2023-08-18 16:26:07 +0900

    Feat: 배너에 react-slick 추가

--- src/components/commons/layout/banner/index.tsx
+++ src/components/commons/layout/banner/index.tsx
@@ -1,11 +1,5 @@
-import styled from "@emotion/styled";
-
-const Wrapper = styled.div`
-  width: 120rem;
-  height: 25rem;
-  background-color: gray;
-`;
+import Item from "./Item";
 
 export default function LayoutBanner() {
-  return <Wrapper>배너입니다.</Wrapper>;
+  return <Item />;
 }
